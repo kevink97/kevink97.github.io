38e91baa5e904bc7c6808b6cf0f223f698bfe2c7
kevink97 <kevinkang06@gmail.com>
2016-09-21 21:15:40 -0700

    minor changes to in typedJS Array

--- script.js
+++ script.js
@@ -3,10 +3,9 @@ $(document).ready(function(){
 });
 
 var typedArray = [
-    "likes to code.^200",
+    "is an explorer.^200",
+    "loves to code.^200",
     "loves to teach.^200",
-    "likes to explore.^200",
-    "is passionate about coding &amp; education.^200",
     "Kang"
 ];
 
